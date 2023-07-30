63d5ba093fa7e0c8bfc0f8fe5ed5a2a82fd694ee
Nikolay-Petrov-Ognyanov <nikolaypetrovognyanov25051995@gmail.com>
2023-07-30 22:25:33 +0300

    fix scrolling to top

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import "./App.css"
-import { Routes, Route } from "react-router-dom"
+import { Routes, Route, useLocation } from "react-router-dom"
 import Footer from "./components/Footer/Footer"
 import Header from "./components/Header/Header"
 import TermsAndConditions from "./components/About/TermsAndContidions"
 import PrivacyPolicy from "./components/About/PrivacyPolicy"
 import ContactUs from "./components/About/ContactUs"
+import { useEffect } from "react"
 
 export default function App() {
+	const { pathname } = useLocation()
+
+	useEffect(() => { setTimeout(() => { window.scrollTo(0, 0) }) }, [pathname])
+
 	return <div className="App">
 		<Header />
 
--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,19 +2,23 @@ import style from "./Footer.module.css"
 import { NavLink } from "react-router-dom"
 
 export default function Footer() {
-    function scrollToTop() { window.scrollTo(0, 0) }
-
     return <footer className={style.footer}> <nav className={style.nav}>
-        <NavLink onClick={scrollToTop} className={style.button} activeclassname="active"
+        <NavLink
             to="/about/terms_and_conditions"
+            className={style.button}
+            activeclassname="active"
         >Terms and Conditions</NavLink>
 
-        <NavLink onClick={scrollToTop} className={style.button} activeclassname="active"
+        <NavLink
             to="/about/privacy_policy"
+            className={style.button}
+            activeclassname="active"
         >Privacy Policy</NavLink>
 
-        <NavLink onClick={scrollToTop} className={style.button} activeclassname="active"
+        <NavLink
             to="/about/contact_us"
+            className={style.button}
+            activeclassname="active"
         >Contact Us</NavLink>
     </nav></footer>
 }
